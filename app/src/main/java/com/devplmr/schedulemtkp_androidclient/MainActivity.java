6cb4d9a986a9968b0d2a65289966bc181fafe5b2
plmr0 <ilya.l2013@yandex.ru>
2019-03-18 00:50:40 +0300

    Alpha Skeleton Additions

--- app/src/main/java/com/devplmr/schedulemtkp_androidclient/MainActivity.java
+++ app/src/main/java/com/devplmr/schedulemtkp_androidclient/MainActivity.java
@@ -1,29 +1,21 @@
 package com.devplmr.schedulemtkp_androidclient;
 
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CompoundButton;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
-
-import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    private TextView mTextMessage;
-    private Button mButtonForFollow;
+    private CompoundButton debugTopicSwitch;
+    private CompoundButton pushTopicSwitch;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener()
@@ -34,18 +26,22 @@ public class MainActivity extends AppCompatActivity
         {
             switch (item.getItemId())
             {
-                case R.id.navigation_home:
-                    mButtonForFollow.setVisibility(View.INVISIBLE);
-                    mTextMessage.setVisibility(View.VISIBLE);
-                    mTextMessage.setText(R.string.navigation_home);
-                    return true;
                 case R.id.navigation_changes:
-                    mTextMessage.setVisibility(View.INVISIBLE);
-                    mButtonForFollow.setVisibility(View.VISIBLE);
+                    debugTopicSwitch.setVisibility(View.INVISIBLE);
+                    pushTopicSwitch.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigation_schedule:
+                    debugTopicSwitch.setVisibility(View.INVISIBLE);
+                    pushTopicSwitch.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigation_debug:
+                    debugTopicSwitch.setVisibility(View.VISIBLE);
+                    pushTopicSwitch.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
         }
+
     };
 
     @Override
@@ -56,10 +52,71 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        mButtonForFollow = (Button) findViewById(R.id.button);
-        mButtonForFollow.setVisibility(View.INVISIBLE);
+        debugTopicSwitch = (CompoundButton) findViewById(R.id.switch1);
+        pushTopicSwitch = (CompoundButton) findViewById(R.id.switch2);
+
+        // Take saved config for "debug" switch
+        SharedPreferences debug_topic_s_p = getSharedPreferences("debug_topic", 0);
+        boolean b1 = debug_topic_s_p.getBoolean("switch_debug_key", false);
+        debugTopicSwitch.setChecked(b1);
+
+        // Take saved config for "push" switch
+        SharedPreferences push_topic_s_p = getSharedPreferences("push_topic", 0);
+        boolean b2 = push_topic_s_p.getBoolean("switch_push_key", false);
+        pushTopicSwitch.setChecked(b2);
+
+        // onCreate Invisibility
+        debugTopicSwitch.setVisibility(View.INVISIBLE);
+        pushTopicSwitch.setVisibility(View.INVISIBLE);
+
+        // Event checkedChangeListener for debugTopicSwitch
+        debugTopicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    FirebaseMessaging.getInstance().subscribeToTopic("debug");
+                }
+                else
+                {
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("debug");
+                }
+                SharedPreferences settings = getSharedPreferences("debug_topic", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("switch_debug_key", isChecked);
+                editor.apply();
+            }
+
+        });
+
+        // Event checkedChangeListener for pushTopicSwitch
+        pushTopicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    FirebaseMessaging.getInstance().subscribeToTopic("push");
+                }
+                else
+                {
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("push");
+                }
+                SharedPreferences settings = getSharedPreferences("push_topic", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("switch_push_key", isChecked);
+                editor.apply();
+            }
+
+        });
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
